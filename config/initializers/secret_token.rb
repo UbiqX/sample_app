5015767cfc9f977eb1a3e305ab35674fa39710e3
UbiqX <ubiqx@robertfrench.ca>
2011-03-12 18:06:44 -0500

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ecadf0ad431178a0185b82b195d8bb1d51d6af370d2ad68cdbf66732a59898b6678ddcec60be20847cf2e2e9dd25e3296c17e05be8bb79d6cf1e72480d339a18'
